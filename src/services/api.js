7ea7d39ec9939e3ad32e128ac0c7624b8045e0e7
YairYahav <yairyahav5@gmail.com>
2025-12-31 13:32:25 +0200

    connecting to netlify

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,16 @@
 import axios from 'axios';
 
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api', // לוודא שזה תואם לפורט של השרת!!!!
+    baseURL: BASE_URL,
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
     }
 });
+
+
 
 // Interceptor עבור הבקשות (Requests) - הזרקת ה-Token
 api.interceptors.request.use(
